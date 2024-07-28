7de41af76fc6d2173c91b9006ae20fa0b28516c7
abhilasha0623 <abhilasha.92.kp@gmail.com>
2024-07-28 22:53:12 +0530

    addComponents

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+  
+  <button @click="toggleDetails">{{ detailsAreVisible?'Hide' : 'Show'}} Details</button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong>{{ friend.phone }} </li>
+    <li><strong>Email:</strong>{{ friend.email }}</li>
+  </ul>
+  </li>
+</template>
+
+<script>
+export default {
+ data () {
+    return {
+      detailsAreVisible:false,
+      friend: {
+       id:"manuel",
+       name:"Manuel Louentx",
+       phone:"12345 87654 908",
+       email:"manuel@localhost.com",
+      },
+    }
+ },
+  methods: {
+  toggleDetails() {
+       this.detailsAreVisible=! this.detailsAreVisible;
+  }
+}
+}
+</script>
